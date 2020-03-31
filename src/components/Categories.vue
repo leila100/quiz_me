ef73b5982d7ae3eb5d97faa7a86fabdc56a49233
Leila <nb.leila10@gmail.com>
2020-03-31 17:05:48 -0500

    Get the categories from the API and display them in categories page

---
+++ src/components/Categories.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>Categories page</h1>
+    <div class="cardsContainer">
+      <div v-for="category in categories" :key="category.id" class="card">
+        <h3 class="title">{{ category.title }}</h3>
+        <div class="numClues">
+          Questions: <span>{{ category.clues_count }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      categories: null
+    };
+  },
+  props: {
+    msg: String
+  },
+  mounted() {
+    axios.get("http://jservice.io/api/categories?count=20").then(response => {
+      console.log(response.data);
+      this.categories = response.data;
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cardsContainer {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: space-between;
+}
+
+.card {
+  width: 400px;
+  border: 2px solid #42b983;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  margin: 10px;
+  /* width: 20%; */
+}
+
+.title {
+  font-size: 22px;
+}
+
+.numClues {
+  font-weight: bold;
+}
+
+.numClues span {
+  color: #42b983;
+}
+</style>
